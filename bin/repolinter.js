1c5ab154dd03725d1feb5dbfa3b788e37e0a42be
andxpto <andrecorreiait@gmail.com>
2020-06-18 16:25:20 +0100

    new arg to choose custom ruleset path

--- bin/repolinter.js
+++ bin/repolinter.js
@@ -7,16 +7,31 @@ const repolinter = require('..')
 
 if (process.argv[2] === '--git') {
   const git = require('simple-git')()
-  const uuidv4 = require('uuid/v4')
+  const { v4: uuidv4 } = require('uuid');
   const rimraf = require('rimraf')
   const tmpDir = path.resolve(process.cwd(), 'tmp', uuidv4())
 
   git.clone(process.argv[3], tmpDir, (error) => {
     if (!error) {
-      repolinter.lint(tmpDir)
+
+      if (process.argv[4] === '--rulefile') {
+        const jsonfile = require('jsonfile')
+        var jsonRuleset = jsonfile.readFileSync(process.argv[5])
+        repolinter.lint(tmpDir, [],  jsonRuleset)
+      }
+      else {
+        repolinter.lint(tmpDir)
+      }
+
     }
     rimraf(tmpDir, function () {})
   })
+} else  if (process.argv[2] === '--rulefile') {
+
+  const jsonfile = require('jsonfile')
+  var jsonRuleset = jsonfile.readFileSync(process.argv[3])
+  repolinter.lint(process.cwd(), [],  jsonRuleset)
+
 } else {
   repolinter.lint(path.resolve(process.cwd(), process.argv[2] || '.'))
 }
